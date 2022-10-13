082beb1b9f5a33a2039164f5be24a8485841d7bd
h-takauma <h-takauma@i-enter.co.jp>
2022-10-13 14:48:53 +0900

    Nodejsインストール

--- liff-v2-handson-playground/foreground.sh
+++ liff-v2-handson-playground/foreground.sh
@@ -2,6 +2,10 @@ git clone https://github.com/gaomar/liff-v2-demo.git example
 
 cd example
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && source ~/.bashrc && nvm install 14.17.3
+
+apt --yes install jq && clear && echo "Welcome to the Pactflow Tutorial, all the dependencies are installed, and you should be good to go!"
+
 npm i
 
 clear
