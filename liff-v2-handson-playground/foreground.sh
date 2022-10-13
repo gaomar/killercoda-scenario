ce827953dcc8fe63a68b887c10543106417b9208
h-takauma <h-takauma@i-enter.co.jp>
2022-10-13 15:47:48 +0900

    コード編集

--- liff-v2-handson-playground/foreground.sh
+++ liff-v2-handson-playground/foreground.sh
@@ -4,7 +4,7 @@ cd example
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && source ~/.bashrc && nvm install 14.17.3
 
-apt --yes install jq && clear && echo "Welcome to the Pactflow Tutorial, all the dependencies are installed, and you should be good to go!"
+apt --yes install jq
 
 npm i
 
