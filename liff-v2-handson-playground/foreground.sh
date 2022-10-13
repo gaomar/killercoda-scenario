704cca1b12d9b240bc4eb603667b633aed936619
h-takauma <h-takauma@i-enter.co.jp>
2022-10-13 15:56:24 +0900

    コード編集

--- liff-v2-handson-playground/foreground.sh
+++ liff-v2-handson-playground/foreground.sh
@@ -4,9 +4,9 @@ cd example
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && source ~/.bashrc && nvm install 14.17.3
 
-apt --yes install jq
-
 npm i
+
+apt --yes install jq && npm i
 
 clear
 
