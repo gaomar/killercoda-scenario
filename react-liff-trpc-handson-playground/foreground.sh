dcae0f97abf8d4c013b4dd0654c077239cbbdc09
h-takauma <h-takauma@i-enter.co.jp>
2023-02-10 17:53:12 +0900

    React x LIFF x tRPC ハンズオン追加

---
+++ react-liff-trpc-handson-playground/foreground.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/gaomar/react-liff-trpc-sample.git example
+
+cd example
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && source ~/.bashrc && nvm install 16.18.1
+
+npm install -g yarn
+
+clear
+
+echo "インストール完了しました！"
+
