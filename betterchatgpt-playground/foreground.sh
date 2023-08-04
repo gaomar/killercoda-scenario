aa2baac5ffb233be1670e8ac324ea0cbbe1945a3
h-takauma <h-takauma@i-enter.co.jp>
2023-08-04 11:03:32 +0900

    2023-08-04.1

---
+++ betterchatgpt-playground/foreground.sh
@@ -0,0 +1,14 @@
+git clone https://github.com/ztjhz/BetterChatGPT.git example
+
+cd example
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && source ~/.bashrc && nvm install 18.16.0
+
+npm install -g yarn
+
+yarn
+
+clear
+
+echo "インストール完了しました！"
+
