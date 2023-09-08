60f4f5376380f7847bc0ec368f84589d7f178cae
gaomar <hirophilip@gmail.com>
2023-09-08 17:13:19 +0900

    Node18

--- node-red-chatgpt-handson-playground/foreground.sh
+++ node-red-chatgpt-handson-playground/foreground.sh
@@ -1,4 +1,4 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && source ~/.bashrc && nvm install 16.20.0
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && source ~/.bashrc && nvm install 18.17.1
 
 npm install -g --unsafe-perm node-red
 
