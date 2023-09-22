75d54f1237ff5f80b56d78bdbc206e05047912c2
h-takauma <h-takauma@i-enter.co.jp>
2023-09-22 18:22:32 +0900

    2023-09-22.1

---
+++ bun-hono-line-handson-playground/foreground.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/gaomar/handson-hono-on-line.git example
+
+cd example
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && source ~/.bashrc && nvm install 18.17.1
+
+clear
+
+echo "インストール完了しました！"
+
