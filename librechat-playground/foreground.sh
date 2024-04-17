96187b1daa936838a48ab2efc838316b8dab36e2
h-takauma <h-takauma@i-enter.co.jp>
2024-04-17 09:27:28 +0900

    LibreChat追加

---
+++ librechat-playground/foreground.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/danny-avila/LibreChat.git example
+
+cd example
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash && source ~/.bashrc && nvm install 20.11.0
+
+cp .env.example .env
+
+clear
+
+echo "インストール完了しました！"
+
---
+++ librechat-playground/intro.md
@@ -0,0 +1,8 @@
+## この教材について
+
+この教材はDifyを実際に動きを確認することができるハンズオンです。
+
+右側の画面に`インストール完了しました！`と表示されていれば、
+START ボタンを押して、はじめましょう！
+
+![s100](https://raw.githubusercontent.com/gaomar/killercoda-scenario/master/betterchatgpt-playground/images/s100.png)
